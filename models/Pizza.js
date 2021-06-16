9514d7311aff89dec582cd608903a06aa54f0a94
Otis Singfield <singfieldotis@gmail.com>
2021-06-16 18:39:05 -0400

    added getters and comment routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
 
+const dateFormat = require('../utils/dateFormat');
+
 const PizzaSchema = new Schema({
   pizzaName: {
     type: String
@@ -9,7 +11,8 @@ const PizzaSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
   },
   size: {
     type: String,
@@ -26,6 +29,7 @@ const PizzaSchema = new Schema({
 {
   toJSON: {
     virtuals: true,
+    getters: true
   },
   id: false
 }
